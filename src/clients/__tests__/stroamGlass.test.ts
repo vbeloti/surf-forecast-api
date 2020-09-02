c84ed946fb8db1fa0939d5e01edc72c2523b8be5
unknown <viniciusbeloti7@gmail.com>
2020-09-02 17:15:47 -0300

    feat: add local response data

--- src/clients/__tests__/stroamGlass.test.ts
+++ src/clients/__tests__/stroamGlass.test.ts
@@ -1,12 +1,19 @@
 import { StormGlass } from '@src/clients/stormGlass';
+import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json';
+import stormGlassNormalized3HoursFixture from '@test/fixtures/stormglass_normalized_response_3_hours.json';
+
+jest.mock('axios');
 
 describe('StormGlass client', () => {
-  it('Should return the normalized forecast from the StormGlassservice ', async () => {
+  it('Should return the normalized forecast from the StormGlas sservice ', async () => {
     const lat = -33.792726;
     const lng = 151.289824;
 
-    const stormGlass = new StormGlass();
+    axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
+
+    const stormGlass = new StormGlass(axios);
     const response = await stormGlass.fetchPoints(lat, lng);
-    expect(response).toEqual({});
+    expect(response).toEqual(stormGlassNormalized3HoursFixture);
   });
 });
